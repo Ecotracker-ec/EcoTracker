957852b68afd3c55d45f7c1898d26cb6ca3162b9
Ecotracker-ec <ecotracker.ec@gmail.com>
2024-07-17 22:58:01 +0530

    Delete backend directory

--- backend/routes/auth.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller =  require('../controllers/usercontroller')
-
-router.post('/register', controller.registerUser)
-router.post('/login', controller.loginUser)
--- backend/routes/calc.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/EmissionController')
-
-router.post('/', controller.createEmission)
-//router.get('/')
