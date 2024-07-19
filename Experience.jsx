bd8f284aeb64cf2c4d27be78e665716ab69732a3
Kunal-8799 <145530521+Kunal-8799@users.noreply.github.com>
2024-07-20 00:31:07 +0530

    Add files via upload
    Made responsive

---
+++ Experience.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const ExperienceCard = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: "#1d1836",
+        color: "#fff",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+    >
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+      </div>
+
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
+
+const Experience = () => {
+  return (
+    <>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+      </div>
+      <div className='bg-primary flex justify-center items-center pb-10'>
+        <div className=' mt-20 md:mt-40'>
+          <motion.div variants={textVariant()}>
+            <h1 className={`${styles.sectionHeadText} text-center`}>
+              FAQs
+            </h1>
+          </motion.div>
+
+          <div className='mt-5 md:mt-20 flex flex-col'>
+            <VerticalTimeline>
+              {experiences.map((experience, index) => (
+                <ExperienceCard
+                  key={`experience-${index}`}
+                  experience={experience}
+                />
+              ))}
+            </VerticalTimeline>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </>
+  );
+};
+
+export default Experience
