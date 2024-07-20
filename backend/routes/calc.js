1cac5a8007939825c3c066ed9e83b829f7e3b005
Ecotracker-ec <ecotracker.ec@gmail.com>
2024-07-20 15:20:21 +0530

    Delete backend directory

--- backend/routes/calc.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { createEmission, getEmission, getALlEmissions } from '../controllers/emissioncontroller.js';
-
-router.post('/', createEmission)
-router.get('/', getEmission)
-//router.get('/')
-export default router
